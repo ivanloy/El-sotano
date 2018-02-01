5135f3d394cb24195e155d62b7e11f131bf5527c
ivanloy <32630838+ivanloy@users.noreply.github.com>
2018-02-01 14:39:55 +0100

    Making doors coincide

--- src/elsotano/mains/Main.java
+++ src/elsotano/mains/Main.java
@@ -16,8 +16,26 @@ public class Main {
 		
 		for(int i = 0; i < 5; i++) {
 			for(int j = 0; j < 5; j++){
-				
+					
 				boolean[] hasDoor = {random.nextBoolean(), random.nextBoolean(), random.nextBoolean(), random.nextBoolean()};
+				
+				if(i == 0) hasDoor[3] = true;
+				else if(i == 4) hasDoor[1] = true;
+				else {
+					
+					//hasDoor[3] = salas[i][j-]
+					
+				}
+				if(j == 0) hasDoor[0] = true;
+				else if(j == 4) hasDoor[2] = true;
+				if(j > 0) {
+					
+					hasDoor[3] = salas[i][j-1].getHasDoor()[1];
+					
+				}
+				
+				
+				
 				float humidity = random.nextFloat()*100;
 				float temperature = random.nextFloat()*40-10;
 				String roomCode = (char)(j + 65) + "" + i;
