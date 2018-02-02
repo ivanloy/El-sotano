1c4a41777130550f18c88ec81638fde8c5067cb6
ivanloy <32630838+ivanloy@users.noreply.github.com>
2018-02-02 14:11:34 +0100

    Door connectiones "fixed"
    Have to change it to graph

--- src/elsotano/mains/Main.java
+++ src/elsotano/mains/Main.java
@@ -21,24 +21,36 @@ public class Main {
 				
 				if(i == 0) hasDoor[3] = true;
 				else if(i == 4) hasDoor[1] = true;
-				else {
+				if(i > 0) {
 					
-					//hasDoor[3] = salas[i][j-]
+					hasDoor[3] = salas[i-1][j].getHasDoor()[1];
 					
 				}
 				if(j == 0) hasDoor[0] = true;
 				else if(j == 4) hasDoor[2] = true;
 				if(j > 0) {
 					
-					hasDoor[3] = salas[i][j-1].getHasDoor()[1];
+					hasDoor[0] = salas[i][j-1].getHasDoor()[2];
 					
 				}
 				
+				if(i == 0 && j == 0) {
+					
+					hasDoor[1] = false;
+					hasDoor[2] = false;
+					
+				}
 				
+				if(i == 4 && j == 4) {
+					
+					hasDoor[3] = false;
+					hasDoor[0] = false;
+					
+				}
 				
 				float humidity = random.nextFloat()*100;
 				float temperature = random.nextFloat()*40-10;
-				String roomCode = (char)(j + 65) + "" + i;
+				String roomCode = (char)(i + 65) + "" + j;
 				
 				salas[i][j] = new Sala(roomCode, hasDoor, temperature, humidity);
 				
